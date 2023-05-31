d9b0b8341ee26af342b670a5190f53bf09df882c
Lumi <shaicloud@gmail.com>
2023-05-31 19:28:01 -0400

    changed stylings

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,34 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./styles/App.css";
 import Header from "./components/Header";
 import Personal from "./components/Personal";
 import Education from "./components/Education";
-import Buttons from "./components/Buttons";
+import Job from "./components/Job";
+import ResetButton from "./components/ResetButton";
+import DisplayCV from "./components/DisplayCV";
 
 function App() {
-	const [formData, setFormData] = useState({
+	const initialFormData = {
 		firstName: "",
 		lastName: "",
 		phoneNumber: "",
 		email: "",
 		schoolName: "",
 		degreeName: "",
-		startYear: "",
-		endYear: "",
-	});
+		schoolStartDate: "",
+		schoolEndDate: "",
+		companyName: "",
+		positionTitle: "",
+		jobStartDate: "",
+		jobEndDate: "",
+	};
 
-	useEffect(() => {
-		console.table(formData);
-	}, []);
+	const [formData, setFormData] = useState(initialFormData);
+	const [isSubmitted, setIsSubmitted] = useState(false);
 
 	const handleFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
 		event.preventDefault();
-		console.table(formData);
+		setIsSubmitted(true);
 	};
 
 	const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -34,26 +39,35 @@ function App() {
 		}));
 	};
 
+	const handleReset = () => {
+		setFormData(initialFormData);
+		setIsSubmitted(false);
+		console.log("reset hit");
+	};
+
 	return (
 		<>
-			<Header></Header>
-			<form className="userForm" onSubmit={handleFormSubmit}>
-				<Personal
-					firstName={formData.firstName}
-					lastName={formData.lastName}
-					phoneNumber={formData.phoneNumber}
-					email={formData.email}
-					handleInputChange={handleInputChange}
-				/>
-				<Education
-					schoolName={formData.schoolName}
-					degreeName={formData.degreeName}
-					startYear={formData.startYear}
-					endYear={formData.endYear}
-					handleInputChange={handleInputChange}
-				/>
-				<Buttons />
-			</form>
+			<div className="header">
+				<Header />
+			</div>
+			{isSubmitted ? ( //if the form state is = submitted, we display the information to the user
+				<div className="submittedInformation">
+					<DisplayCV formData={formData} />
+					<ResetButton handleReset={handleReset} />
+				</div>
+			) : (
+				//if not submitted, we continue to display the form
+				<form className="userForm" onSubmit={handleFormSubmit}>
+					<Personal formData={formData} handleInputChange={handleInputChange} />
+					<Education formData={formData} handleInputChange={handleInputChange} />
+					<Job formData={formData} handleInputChange={handleInputChange} />
+
+					<div className="buttonGroup">
+						<button type="submit">Submit</button>
+						<ResetButton handleReset={handleReset} />
+					</div>
+				</form>
+			)}
 		</>
 	);
 }
